556526e3c8c18b81effc7d0f8bda5579186c9be2
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-13 09:15:55 +0200

    finished Area of a Circle kata

---
+++ src/Circle.java
@@ -0,0 +1,6 @@
+public class Circle {
+    public static double area(double radius) {
+        if (radius <= 0) throw new IllegalArgumentException();
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
