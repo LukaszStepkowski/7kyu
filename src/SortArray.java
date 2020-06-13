779c941b14da33081de12d693d6aaf3eaa98c90b
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-13 07:34:56 +0200

    finished Sort arrays - 1 kata

---
+++ src/SortArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static int[] sortArray(int[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static Integer[] sortArray(Integer[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static long[] sortArray(long[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static double[] sortArray(double[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static float[] sortArray(float[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static String[] sortArray(String[] names){
+        Arrays.sort(names);
+        return names;
+    }
+}
