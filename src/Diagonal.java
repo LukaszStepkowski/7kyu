11969bbc644f9a01ddc7c5590bbb384bc8a6b005
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-23 07:28:44 +0200

    finished Find sum of top-left to bottom-right diagonals kata

---
+++ src/Diagonal.java
@@ -0,0 +1,9 @@
+public class Diagonal {
+    public static int diagonalSum(final int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+        return sum;
+    }
+}
